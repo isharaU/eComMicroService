142b90d0bd17eb81274a88d2d7eab5a0e20200fd
isharaU <wellahewawiud.20@uom.lk>
2025-07-03 23:03:20 +0530

    add CreateProduct service

--- product-service/src/main/java/com/ishara/product_service/service/ProductService.java
+++ product-service/src/main/java/com/ishara/product_service/service/ProductService.java
@@ -1,9 +1,24 @@
 package com.ishara.product_service.service;
 
+import com.ishara.product_service.dto.ProductRequest;
 import com.ishara.product_service.model.Product;
+import com.ishara.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
-    public void CreateProduct() {
+    private final ProductRepository productRepository;
 
+    public void CreateProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .price(productRequest.getPrice())
+                .name(productRequest.getName())
+                .description(productRequest.getDescription()).build();
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
     }
 }

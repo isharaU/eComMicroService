bb03c4e08ec0928b7cd974bfdfa55cc0f74350d7
isharaU <wellahewawiud.20@uom.lk>
2025-07-03 23:06:44 +0530

    add createProduct controller

--- product-service/src/main/java/com/ishara/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ishara/product_service/controller/ProductController.java
@@ -1,20 +1,27 @@
 package com.ishara.product_service.controller;
 
+import com.ishara.product_service.dto.ProductRequest;
+import com.ishara.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct() {
-
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.CreateProduct(productRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public void getAllProduct() {
+
 
     }
 }

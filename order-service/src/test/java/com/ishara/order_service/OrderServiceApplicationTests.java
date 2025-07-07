719aadd48f814583b52b421b11cd126936ae4454
isharaU <wellahewawiud.20@uom.lk>
2025-07-07 15:26:34 +0530

    initiate order service

---
+++ order-service/src/test/java/com/ishara/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishara.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

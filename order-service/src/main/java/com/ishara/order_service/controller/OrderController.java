205e958c4910de4577927b979a0f0bcd7768b82f
isharaU <wellahewawiud.20@uom.lk>
2025-07-07 16:38:43 +0530

    inject orderService into the controller

--- order-service/src/main/java/com/ishara/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/ishara/order_service/controller/OrderController.java
@@ -1,15 +1,23 @@
 package com.ishara.order_service.controller;
 
+import com.ishara.order_service.dto.OrderRequest;
+import com.ishara.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/products")
+@RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
     @PostMapping
-    private String placeOrder() {
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
         return "Order is successfully Placed.";
     }
 }

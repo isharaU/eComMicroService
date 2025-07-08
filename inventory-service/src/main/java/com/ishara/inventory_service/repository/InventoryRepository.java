5e8a8428939a732e72b049a1b4b8682926f87cf1
isharaU <wellahewawiud.20@uom.lk>
2025-07-08 17:26:02 +0530

    add service and repository alongside with the controller

--- inventory-service/src/main/java/com/ishara/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/ishara/inventory_service/repository/InventoryRepository.java
@@ -3,5 +3,8 @@ package com.ishara.inventory_service.repository;
 import com.ishara.inventory_service.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
 }

ed2a15e5e60ba6c825b823a95de84be407b330ec
isharaU <wellahewawiud.20@uom.lk>
2025-07-08 17:12:08 +0530

    add InventoryRepository

---
+++ inventory-service/src/main/java/com/ishara/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,7 @@
+package com.ishara.inventory_service.repository;
+
+import com.ishara.inventory_service.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+}

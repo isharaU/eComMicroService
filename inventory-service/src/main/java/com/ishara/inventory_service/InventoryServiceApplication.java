404613b6c5859c9d908096d8a449776d337ae72c
isharaU <wellahewawiud.20@uom.lk>
2025-07-17 18:39:21 +0530

    remove bean to add automatic data for avoid duplicates

--- inventory-service/src/main/java/com/ishara/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ishara/inventory_service/InventoryServiceApplication.java
@@ -1,11 +1,7 @@
 package com.ishara.inventory_service;
 
-import com.ishara.inventory_service.model.Inventory;
-import com.ishara.inventory_service.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -13,21 +9,4 @@ public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
-        return args -> {
-            Inventory inventoryOne = new Inventory();
-            inventoryOne.setSkuCode("Samsung A55");
-            inventoryOne.setQuantity(100);
-
-            Inventory inventoryTwo = new Inventory();
-            inventoryTwo.setSkuCode("I phone 13");
-            inventoryTwo.setQuantity(300);
-
-            inventoryRepository.save(inventoryOne);
-            inventoryRepository.save(inventoryTwo);
-        };
-    }
-
 }

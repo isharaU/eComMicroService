27d6835da87c59df83ad67b6290e35cfc241d999
isharaU <wellahewawiud.20@uom.lk>
2025-07-27 16:11:58 +0530

    remove circuit breaker

--- order-service/src/main/java/com/ishara/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/ishara/order_service/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.ishara.order_service.controller;
 
 import com.ishara.order_service.dto.OrderRequest;
 import com.ishara.order_service.service.OrderService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +15,8 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order is successfully Placed.";
-    }
-
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
-        return "Can not call to the inventory";
     }
 }
